27b1f54d69b8ed3488f5249c0c760e0cd2770bba
Mikhail Glushenkov <mikhail.glushenkov@gmail.com>
2015-12-13 18:12:51 +0100

    Add a test for #1.

---
+++ test/Test.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Main
+       where
+
+import Control.Monad
+
+import Test.HUnit
+import Text.RawString.QQ
+
+multilineUnixNewlines :: String
+multilineUnixNewlines = [r|FOO
+BAR|]
+
+multilineWindowsNewlines :: String
+multilineWindowsNewlines = [r|FOO
+BAR|]
+
+main :: IO ()
+main = void . runTestTT . test $ [
+  "Windows newlines" ~: (multilineUnixNewlines ~=? multilineWindowsNewlines)
+  ]
